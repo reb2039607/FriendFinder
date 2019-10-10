947bb7fc330cc6c8a6b23f23a73c2206a5b0e688
reb2039607 <reb.aguayo@gmail.com>
2019-10-09 23:55:40 -0700

    created survey page, pending actual functionality

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 const express = require('express')
-const app = express()
- 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
-app.listen(3000)
+const app = express();
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 3000;
+
+// app/json parser
+const jsonParser = bodyParser.json();
+
+//urlencoded parser
+const urlencodedParser = bodyParser.urlencoded({ extended: false});
+
+app.use(bodyParser.json({ type: "application/++json" }));
+app.use(bodyParser.raw({ type: "application/vnd.custom-type" }));
+app.use(bodyParser.text({ type: "text/html" }));;
+
+
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
+
+
+//will need to install these, working on no internet currently
+//WILL NEED TO DO NPM INIT FOR PACKAGE.JSON!
 
