1de550ecc18bcdc9e267d530f9103853ed047ebc
reb2039607 <reb.aguayo@gmail.com>
2019-10-14 13:19:12 -0700

    updated / route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,6 +5,10 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname + "/../public/survey.html"));
     });
 
+    app.get("/", function(req, res) {
+        res.json(path.join(__dirname, "/../public/home.html"));
+      });
+
     app.use(function(req, res) {
         res.sendFile(path.join(__dirname + "/../public/home.html"));
     });
